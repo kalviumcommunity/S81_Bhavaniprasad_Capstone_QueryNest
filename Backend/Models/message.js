861b93152c8dfd2451ae019cc52c94df6c0aad59
ybhavaniprasad79 <ybhavaniprasad79@gmail.com>
2025-05-05 13:26:04 +0530

    relationship between entities in database

--- Backend/Models/message.js
+++ Backend/Models/message.js
@@ -1,35 +1,31 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema(
-  {
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
-      required: true
+    {
+      sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+      },
+      commenter: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+      content: {
+        type: String,
+        required: true
+      },
+      upvote: {
+          type:Number,
+      },
+      share: {
+          type:Number,
+      }
+      
     },
-    commenter: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
-    },
-    content: {
-      type: String,
-      required: true
-    },
-    // isRead: {
-    //   type: Boolean,
-    //   default: false
-    // }
-    upvote: {
-        type:Number,
-    },
-    share: {
-        type:Number,
+    {
+      timestamps: true // adds createdAt and updatedAt
     }
-    
-  },
-  {
-    timestamps: true // adds createdAt and updatedAt
-  }
-);
+  );
 
 module.exports = mongoose.model('Message', messageSchema);
